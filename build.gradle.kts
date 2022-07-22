426c0a01801b750377d6e5373b0fc41e1bc0d7df
YoshikazuHosono <yyhhwork@gmail.com>
2022-07-22 12:30:24 +0900

    add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,11 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	// dgs framework
+	implementation(platform("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:latest.release"))
+	implementation("com.netflix.graphql.dgs:graphql-dgs-spring-boot-starter")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
